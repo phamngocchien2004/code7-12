1444cfb191b44aff32bfdfc8b2debd74e1df687d
phamngocchien2004@@ <114908347+phamngocchien2004@users.noreply.github.com>
2022-12-07 15:28:23 +0700

    Create app2.js

---
+++ js/app2.js
@@ -0,0 +1,10 @@
+var validationApp = angular.module('validationApp',[]);
+
+// Create angular controller
+validationApp.controller('mainController', function ($scope) {
+    $scope.submitForm = function (isValid) {
+        if (isValid) {
+            alert('form is Valid');
+        }
+    };
+});
